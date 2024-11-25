de74e64d6af531985715a8ba1aed3969af283e7d
ernestoadriano <140088177+ernestoadriano@users.noreply.github.com>
2024-11-25 22:28:16 +0200

    Initial commit

---
+++ src/main/java/com/ernesto/banco_multiplo/BancoMultiploApplication.java
@@ -0,0 +1,13 @@
+package com.ernesto.banco_multiplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoMultiploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoMultiploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ernesto/banco_multiplo/BancoMultiploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernesto.banco_multiplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoMultiploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
